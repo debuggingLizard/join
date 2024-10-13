d1903ef65f6bc1b111552f42e9eb38e75b199b32
Lisa-Marie Maliga <lisa.maliga@outlook.com>
2024-10-13 15:06:46 +0200

    implmented contact list render functionality

--- assets/js/contact-list.js
+++ assets/js/contact-list.js
@@ -5,6 +5,7 @@ async function renderContactList() {
     // console.log(users);
     getFirstLetter(users);
     // console.log(groupedUsers);
+    displayContactList();
 }
 
 function getFirstLetter(users) {
@@ -20,6 +21,24 @@ function getFirstLetter(users) {
 
 function displayContactList() {
     Object.keys(groupedUsers).forEach(letter => {
-        
-    })
+        let listRef = document.getElementById('contact-list-list');
+        listRef.innerHTML += /*html*/`
+            <div id="contact-list-item-${letter}" class="contact-list-item">
+                    <div class="contact-list-letter">${letter}</div>
+                    <div class="contact-list-hr"></div>
+            </div>
+        `
+        groupedUsers[letter].forEach(user => {
+            let letterRef = document.getElementById('contact-list-item-' + letter);
+            letterRef.innerHTML += /*html*/`
+                <div id="contact-list-user-${user.id}" class="contact-list-contact" onclick="contactListUpdateActiveState()">
+                        <div class="contact-list-profile">AM</div>
+                        <div class="contact-list-contact-info">
+                            <p>${user.name}</p>
+                            <a href="mailto:${user.email}">${user.email}</a>
+                        </div>
+                    </div>
+            `
+        });
+    });
 }
