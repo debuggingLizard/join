5e2d06819387ab03c4368a2dceb542ef5caf1af6
Lisa-Marie Maliga <lisa.maliga@outlook.com>
2024-10-13 14:45:09 +0200

    extracted first letter for contact grouping

--- assets/js/contact-list.js
+++ assets/js/contact-list.js
@@ -1,5 +1,25 @@
+let groupedUsers = {};
+
 async function renderContactList() {
-    // let users = await getData('users');
+    let users = await getData('users');
     // console.log(users);
-    
+    getFirstLetter(users);
+    // console.log(groupedUsers);
+}
+
+function getFirstLetter(users) {
+    Object.keys(users).forEach(key => {
+        let user = users[key];
+        let firstLetter = user.name[0].toUpperCase();
+        if (!groupedUsers[firstLetter]) {
+            groupedUsers[firstLetter] = [];
+        };
+        groupedUsers[firstLetter].push(user);
+    });
+}
+
+function displayContactList() {
+    Object.keys(groupedUsers).forEach(letter => {
+        
+    })
 }
