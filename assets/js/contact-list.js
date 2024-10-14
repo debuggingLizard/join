79ef500ca95c6ba07ffae3c504b6a3871b46ff62
Lisa-Marie Maliga <lisa.maliga@outlook.com>
2024-10-14 17:44:02 +0200

    updated ID render for users

--- assets/js/contact-list.js
+++ assets/js/contact-list.js
@@ -29,16 +29,10 @@ function getFirstLetters(users) {
   sortedUsers.forEach((key) => {
     let user = users[key];
     let firstLetter = user.name[0].toUpperCase();
-    let initials = user.name.split(" ").map((name, index, array) => {
-        if (index === 0 || index === array.length - 1) {
-          return name[0].toUpperCase();
-        }
-        return "";
-      }).join("");
     if (!groupedUsers[firstLetter]) {
       groupedUsers[firstLetter] = [];
     }
-    groupedUsers[firstLetter].push({ ...user, initials });
+    groupedUsers[firstLetter].push({ ...user, key});
   });
 }
 
@@ -82,8 +76,8 @@ function getLetterGroupTemplate(letter) {
  */
 function getUserContactListTemplate(user) {
   return /*html*/ `
-        <div id="contact-list-user-${user.id}" class="contact-list-contact" onclick="console.log('Open Contact with id ${user.id}')">
-                        <div class="contact-list-profile" style="background-color: ${user.color};">${user.initials}</div>
+        <div id="contact-list-user-${user.key}" class="contact-list-contact" onclick="console.log('Open Contact with id ${user.key}')">
+                        <div class="contact-list-profile" style="background-color: ${user.color};">${user.profileImage}</div>
                         <div class="contact-list-contact-info">
                             <p>${user.name}</p>
                             <a href="mailto:${user.email}">${user.email}</a>
