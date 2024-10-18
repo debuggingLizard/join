b5d636762201a3a3c73fde1b42238f14e8230b05
M. W. <meriem.ben@gmx.de>
2024-10-18 10:56:48 +0200

    Button Funktionalität
    Funktionalität bei klicken hinzugefügt.

---
+++ assets/js/addTask.js
@@ -0,0 +1,8 @@
+function selectPrio(selected) {
+  document.querySelectorAll(".prio-btn").forEach((button) => {
+    button.classList.remove("active");
+  });
+  document.querySelector(`.prio-btn.${selected}`).classList.add("active");
+  document.getElementById("selectedPrio").value = selected;
+}
+selectPrio("medium");
