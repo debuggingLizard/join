762ed691c6705ac04c3c47ab025474160a930fd0
Lisa-Marie Maliga <lisa.maliga@outlook.com>
2024-10-18 13:04:37 +0200

    small fixes for task form + added active state functionality for all sidebar links

--- assets/js/addTask.js
+++ assets/js/addTask.js
@@ -1,11 +1,12 @@
 /**
- * Sets the priority (urgent, medium, low), updates the UI, and stores the selection in a hidden field.
+ * Sets the selected priority button as active and removes the active class from others.
+ * 
+ * @param {string} selected - The class name of the selected priority button.
  */
 function selectPrio(selected) {
   document.querySelectorAll(".prio-btn").forEach((button) => {
     button.classList.remove("active");
   });
   document.querySelector(`.prio-btn.${selected}`).classList.add("active");
-  document.getElementById("selectedPrio").value = selected;
+  // document.getElementById("selectedPrio").value = selected;
 }
-selectPrio("medium");
--- script.js
+++ script.js
@@ -20,10 +20,10 @@ function getSidebarTemplate() {
             <img src="./assets/img/JoinLogo.svg" alt="JoinLogo">
         </div>
         <nav>
-            <a href="#" id="sidebar-nav-summary" class="sidebar-nav-a"><img src="./assets/buttons/Summary.svg" alt="SummaryButton"><span>Summary</span></a>
-               <a href=" #" id="sidebar-nav-task" class="sidebar-nav-a"><img src="./assets/buttons/AddTask.svg" alt="AddTask"><span>Add Task</span></a>
-               <a href="#" id="sidebar-nav-board" class="sidebar-nav-a"><img src="./assets/buttons/Board.svg" alt="Board"><span>Board</span></a>
-              <a href="contacts.html" id="sidebar-nav-contacts" class="sidebar-nav-a"><img src="./assets/buttons/Contacts.svg" alt="Contacts"><span>Contacts</span></a>
+            <a href="index.html" id="sidebar-nav-summary" class="sidebar-nav-a"><img src="./assets/buttons/Summary.svg" alt="SummaryButton"><span>Summary</span></a>
+            <a href="addTask.html" id="sidebar-nav-task" class="sidebar-nav-a"><img src="./assets/buttons/AddTask.svg" alt="AddTask"><span>Add Task</span></a>
+            <a href="board.html" id="sidebar-nav-board" class="sidebar-nav-a"><img src="./assets/buttons/Board.svg" alt="Board"><span>Board</span></a>
+            <a href="contacts.html" id="sidebar-nav-contacts" class="sidebar-nav-a"><img src="./assets/buttons/Contacts.svg" alt="Contacts"><span>Contacts</span></a>
         </nav>
         <footer>
             <a tabindex="0" href="#">Privacy Policy</a>
