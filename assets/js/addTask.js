829283f4ed977f232968f01c959f61f1ca758d9c
M. W. <meriem.ben@gmx.de>
2024-10-18 11:00:41 +0200

    Comment Function

--- assets/js/addTask.js
+++ assets/js/addTask.js
@@ -1,3 +1,6 @@
+/**
+ * Sets the priority (urgent, medium, low), updates the UI, and stores the selection in a hidden field.
+ */
 function selectPrio(selected) {
   document.querySelectorAll(".prio-btn").forEach((button) => {
     button.classList.remove("active");
