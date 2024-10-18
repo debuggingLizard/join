46178d835dd605d8533ce6fe7df87064e1929b46
Lisa-Marie Maliga <lisa.maliga@outlook.com>
2024-10-18 17:40:35 +0200

    merge fixes

--- assets/js/task-view.js
+++ assets/js/task-view.js
@@ -1,33 +1,29 @@
-
-
 function openTaskDetail() {
-    const overlay = document.getElementById('overlay');
-    const taskDetail = document.getElementById('task-detail');
-    overlay.style.display = 'flex'; 
-    setTimeout(() => {
-        taskDetail.classList.add('show'); 
-    }, 10); 
+  const overlay = document.getElementById("overlay");
+  const taskDetail = document.getElementById("task-detail");
+  overlay.style.display = "flex";
+  setTimeout(() => {
+    taskDetail.classList.add("show");
+  }, 10);
 }
 
 function closeTaskDetail() {
-    const taskDetail = document.getElementById('task-detail');
-    taskDetail.classList.remove('show'); 
-    setTimeout(() => {
-        document.getElementById('overlay').style.display = 'none'; 
-    }, 300); 
+  const taskDetail = document.getElementById("task-detail");
+  taskDetail.classList.remove("show");
+  setTimeout(() => {
+    document.getElementById("overlay").style.display = "none";
+  }, 300);
 }
-document.getElementById('overlay').addEventListener('click', function() {
-    closeTaskDetail(); 
+document.getElementById("overlay").addEventListener("click", function () {
+  closeTaskDetail();
 });
-document.getElementById('close-btn-task-detail').addEventListener('click', function(event) {
+document
+  .getElementById("close-btn-task-detail")
+  .addEventListener("click", function (event) {
     event.stopPropagation();
-    closeTaskDetail(); 
-});
-window.onload = function() {
-    document.getElementById('overlay').style.display = 'none';
-    document.getElementById('task-detail').classList.remove('show'); 
+    closeTaskDetail();
+  });
+window.onload = function () {
+  document.getElementById("overlay").style.display = "none";
+  document.getElementById("task-detail").classList.remove("show");
 };
-
-
-  
-  
